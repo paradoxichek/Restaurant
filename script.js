22bfa0b4d28e78448c49efaf1eb2cb2efb3cf4a1
paradoxichek <uamir7220@gmail.com>
2024-03-17 23:34:46 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector('.menu_burger_items')
+let buttonBurger = document.querySelector('.menu_burger')
+
+buttonBurger.onclick = () => {
+    console.log('amir');
+burger.classList.toggle('dispp')
+}
