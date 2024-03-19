b72113e66f00a4e937c6024ef80a692fb6028566
paradoxichek <uamir7220@gmail.com>
2024-03-19 15:22:48 +0500

    Full Project

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-let burger = document.querySelector('.menu_burger_items')
-let buttonBurger = document.querySelector('.menu_burger')
+let burger = document.querySelector(".menu_burger_items");
+let buttonBurger = document.querySelector(".menu_burger");
+let buttonModal = document.querySelector("#modal");
+let modalMenu = document.querySelector(".modal");
+let modalBg = document.querySelector(".modal_bg");
+let closeModal = document.querySelector(".cross");
+
+
+
+
 
 buttonBurger.onclick = () => {
-    console.log('amir');
-burger.classList.toggle('dispp')
-}
+  burger.classList.toggle("dispp");
+};
 
-let buttonModal = document.querySelectorAll('#modal')
-let modalMenu = document.querySelector('.modal')
-let modalBg = document.querySelector('.modal_bg')
-let closeModal = document.querySelector('.cross')
-
-buttonModal.forEach(item => {
-    item.onclick = () => {
-     modalMenu.style.top = '50%'
-     modalBg.style.top = '0%'
-     document.body.classList.add('no_scroll')
-    }
-    
-});
+buttonModal.onclick = () => {
+  modalMenu.style.top = "50%";
+  modalBg.style.top = "0%";
+  document.body.classList.add("no_scroll");
+};
 
 closeModal.onclick = () => {
-    modalMenu.style.top = '-50%'
-    modalBg.style.top = '-100%'
-    document.body.classList.remove('no_scroll')
-   }
-modalBg.onclick = () => {
-    modalMenu.style.top = '-50%'
-    modalBg.style.top = '-100%'
-    
-    document.body.classList.remove('no_scroll')
-    
-   }
+  modalMenu.style.top = "-50%";
+  modalBg.style.top = "-100%";
+  document.body.clas;
 
+  sList.remove("no_scroll");
+};
+modalBg.onclick = () => {
+  modalMenu.style.top = "-50%";
+  modalBg.style.top = "-100%";
+  document.body.classList.remove("no_scroll");
+};
