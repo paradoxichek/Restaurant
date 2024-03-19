c989da6f8bd202c3990ba0f286ae7685e91da45f
paradoxichek <uamir7220@gmail.com>
2024-03-19 15:03:08 +0500

    Full Project

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ buttonBurger.onclick = () => {
     console.log('amir');
 burger.classList.toggle('dispp')
 }
+
+let buttonModal = document.querySelectorAll('#modal')
+let modalMenu = document.querySelector('.modal')
+let modalBg = document.querySelector('.modal_bg')
+let closeModal = document.querySelector('.cross')
+
+buttonModal.forEach(item => {
+    item.onclick = () => {
+     modalMenu.style.top = '50%'
+     modalBg.style.top = '0%'
+     document.body.classList.add('no_scroll')
+    }
+    
+});
+
+closeModal.onclick = () => {
+    modalMenu.style.top = '-50%'
+    modalBg.style.top = '-100%'
+    document.body.classList.remove('no_scroll')
+   }
+modalBg.onclick = () => {
+    modalMenu.style.top = '-50%'
+    modalBg.style.top = '-100%'
+    
+    document.body.classList.remove('no_scroll')
+    
+   }
+
